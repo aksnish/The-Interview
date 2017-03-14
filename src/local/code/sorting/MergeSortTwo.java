255019a7976c82d3dd406fb152452691b4a2bf5d
Nishita Jaykumar <nishita@knoesis.org>
2017-03-14 17:16:04 -0400

    new sorting algorithms

---
+++ src/local/code/sorting/MergeSortTwo.java
@@ -0,0 +1,61 @@
+package local.code.sorting;
+
+import java.util.Arrays;
+
+public class MergeSortTwo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {3, 1, 9, 5, 12, 11, 2, 4, 15, 20, 25};
+		System.out.println(Arrays.toString(arr));
+		mergeSort(arr,0,arr.length-1);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public static void mergeSort(int[] arr, int lowerIndex, int upperIndex) {
+		if (lowerIndex >= upperIndex) return;
+		else {
+			int midIndex = (lowerIndex + upperIndex)/2;
+			mergeSort(arr, lowerIndex, midIndex);
+			mergeSort(arr, midIndex+1, upperIndex);
+			merge(arr, lowerIndex, midIndex, upperIndex);
+		}
+	}
+
+	public static void merge(int[] arr, int lowerIndex, int midIndex, int upperIndex) {
+		// TODO Auto-generated method stub
+		int tempArr[] = new int[arr.length];
+		for (int i=lowerIndex; i<=upperIndex; i++) {
+			tempArr[i] = arr[i];
+		}
+		int i = lowerIndex;
+		int j = midIndex+1;
+		int k = lowerIndex;
+		while (i<=midIndex && j <=upperIndex) {
+			if (tempArr[i] <= tempArr[j]) {
+				arr[k] = tempArr[i];
+				i++;
+			}
+			else {
+				arr[k] = tempArr[j];
+				j++;
+			}
+			k++;
+		}
+		// Any leftovers in the lower array
+		while (i<=midIndex) {
+			arr[k] = tempArr[i];
+			k++;
+			i++;
+		}
+		// Any leftovers in upper array - NOT required as whole array is already copied
+		while (j<=upperIndex) {
+			arr[k] = tempArr[j];
+			k++;
+			j++;
+		}
+	}
+}
---
+++ src/local/code/sorting/Mergesort.java
@@ -0,0 +1,66 @@
+package local.code.sorting;
+
+import java.util.Arrays;
+
+public class Mergesort {
+
+	public static void main(String[] args) {
+		//int [] array = {2,8,9,7,6,12,3,5,4,10,11,1};
+		int [] array = {2,8,9,7,6};
+		mergesort(array);
+		
+		for(int i = 0; i < array.length; i++){
+			System.out.print(array[i] + " ");
+		}
+	}
+
+	public static void mergesort(int[] array){
+		mergesort(array, new int[array.length], 0, array.length-1);
+
+	}
+
+	private static void mergesort(int[] array, int[] temp, int leftStart, int rightEnd) {
+		if(leftStart >= rightEnd){
+			return;
+		}
+		int middle = (leftStart + rightEnd)/2;
+		mergesort(array, temp, leftStart, middle);
+		mergesort(array, temp, middle + 1, rightEnd);
+		mergeHalves(array, temp, leftStart, rightEnd );
+	}
+
+	private static void mergeHalves(int[] array, int[] temp, int leftStart, int rightEnd) {
+		int leftEnd = (rightEnd + leftStart)/2;
+		int rightStart = leftEnd + 1;
+		//size of entire array
+		int sizeofArray = rightEnd - leftStart + 1;
+
+		//left index to walk to right half
+		int left = leftStart;
+
+		//right index to walk to right half
+		int right = rightStart;
+
+		//index for the temp array to write elements to this temp array, starting @ leftStart
+		int index = leftStart;
+
+		//this is walking through each half of the element and copying the small element
+		while(left <= leftEnd && right <= rightEnd){
+			if(array[left] <= array[right]){
+				temp[index] = array[left];
+				left++;
+			}
+			else{
+				temp[index] = array[right];
+				right++;
+			}
+			index++;
+		}
+
+		//once one of the indexes goes out of bounds, copy over the rest of the elements in either half
+
+		System.arraycopy(array, left, temp, index, leftEnd - left +1);
+		System.arraycopy(array, right, temp, index, rightEnd - right + 1);
+		System.arraycopy(array, leftStart, temp, leftStart, sizeofArray);
+	}
+}
