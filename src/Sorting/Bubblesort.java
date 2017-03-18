4cadc3dd907729f86938233a8141354428156d2d
Nishita Jaykumar <nishita@knoesis.org>
2017-03-18 16:57:44 -0400

    minor change

--- src/Sorting/Bubblesort.java
+++ src/Sorting/Bubblesort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Bubblesort {
 	public static void main(String[] args) {
-		int array[] = {9,2,6,4,3,5,1};
+		int array[] = {3,2,1};
 		bubblesort(array);
 		for(int i = 0; i < array.length; i++){
 			System.out.print(array[i] + " ");
@@ -13,16 +13,20 @@ public class Bubblesort {
 
 	private static int[] bubblesort(int[] array) {
 		int temp = 0;
+		int num =0;
 		for(int i = 0; i< array.length - 1; i++){
 			for(int j = 1; j < array.length - i; j++){
 				if(array[j - 1] > array[j]){
 					temp = array[j -1];
 					array[j - 1] = array[j];
 					array[j] = temp;
+					num++;
 				}
 			}
-			System.out.println("Iteration " + (i + 1) + ": " + Arrays.toString(array));
+			System.out.println("Iteration "+(i+1)+":"+Arrays.toString(array));
+			
 		}
+		System.out.println(num);
 		 return array;
 	}
 }
